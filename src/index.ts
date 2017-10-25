e452070cd66ad2731912a44ca678ae8e0ae693bf
Thabang Gideon Magaola <thabang@projectcodex.co>
2017-10-25 15:46:39 +0200

    added new Entities

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { Converter } from "csvtojson";
 
-createConnection().then(async connection => {
+import {Limits} from "./entity/Limits";
+import {Relationships} from "./entity/Relationships";
+
+createConnection({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "1",
+    database: "InvestecDB",
+    entities: [
+        Relationships,
+        Limits
+    ],
+    synchronize: true,
+    logging: false
+}).then(async connection => {
 
     console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-    
+    // const user = new User();
+    // user.firstName = "Timber";
+    // user.lastName = "Saw";
+    // user.age = 25;
+    // await connection.manager.save(user);
+    // console.log("Saved a new user with id: " + user.id);
+
     console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
+    const users = await connection.manager.find(Limits);
     console.log("Loaded users: ", users);
-     
+
     console.log("Here you can setup and run express/koa/any other framework.");
-    
+
 }).catch(error => console.log(error));

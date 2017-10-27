db0f8b734c6c0a9c738cff7acf4d3f9ec67d767f
Thabang Gideon Magaola <thabang@projectcodex.co>
2017-10-27 19:07:09 +0200

    Modeified existing code

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,7 @@ createConnection({
 }).then(async connection => {
     var converter = new Converter({});
     var manager = connection.getRepository(Relationships);
-
-    // Delete all table contents to avoid duplicates
+    var limitManager = connection.getRepository(Limits);
 
     converter.fromFile("/home/bootcamp/projects/InvestecDB/src/csv/Entity_Relationships.csv", async (err, results) => {
         await manager.query("DELETE FROM relationships;");
@@ -33,8 +32,7 @@ createConnection({
         console.log("Inserting new data into the Relationships table....");
 
         for (let i = 0; i < results.length; i++) {
-            let relationship = new Relationships();
-            var entity = results[i];
+            let relationship = new Relationships(), entity = results[i];
 
             relationship.Parent_Entity_Id = parseInt(entity["Parent Entity Id"]);
             relationship.Parent_Entity_Name = entity["Parent Entity Name"];
@@ -42,11 +40,47 @@ createConnection({
             relationship.Entity_Id = parseInt(entity["Entity Id"]);
             relationship.Entity_Name = entity["Entity Name"];
 
-            // console.log('relationship', relationship)
             manager.save(relationship).then((result: any) => {
                 console.log("Result:", result)
             }).catch(error => console.log('duplicates'));
         }
-        console.log("Entity has been saved");
+        console.log("Entity Relationship table data saved");
     });
+
+    // Limits Table
+    converter.fromFile("/home/bootcamp/projects/InvestecDB/src/csv/Entity_Limits.csv", async (err: any, results: any) => {
+        await limitManager.query("DELETE FROM limits;");
+        console.log("Deleted All Existing Data from Limits table.");
+
+        if (err) console.log(err);
+
+        console.log("Inserting new data into the Limits table....");
+
+        for (let i = 0; i < results.length; i++) {
+            let limit = new Limits(), entity = results[i];
+
+            limit.Entity_Id = parseInt(entity["Entity Id"]);
+            limit.Risk_Taker_Group_Name = entity["Risk Taker Group Name"];
+            limit.Risk_Taker_Name = entity["Risk Taker Name"];
+            limit.Facility_Id = parseInt(entity["Facility Id"]);
+            limit.Facility_Type = entity["Facility Type"];
+            limit.Limit_Id = parseInt(entity["Limit Id"]);
+            limit.Limit_Type = entity["Limit Type"];
+            limit.Product = entity["Product"];
+            limit.Risk_Type = entity["Risk Type"];
+            limit.Currency = entity["Currency"];
+            limit.Exposure_Amount = parseFloat(entity["Exposure Amount"]);
+            limit.Total_Current_Limit = parseInt(entity["Total Current Limit"]);
+            limit.Total_Approved_Limit = parseInt(entity["Total Approved Limit"]);
+
+            console.log(limit);
+
+          //  limitManager.save(limit).then((result: any) => {
+           //     console.log("Result:", result)
+           // }).catch(error => console.log('Duplicates'));
+        }
+        console.log("Entity Limits table data saved.");
+    });
+
+
 }).catch(error => console.log(error));

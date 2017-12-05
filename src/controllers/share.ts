69021f739ba23642848e7be282b0d755950817e2
Thabang Gideon Magaola <thabang@projectcodex.co>
2017-12-05 11:33:48 +0200

    added id

--- src/controllers/share.ts
+++ src/controllers/share.ts
@@ -4,22 +4,20 @@ import { getRepository, getManager } from 'typeorm';
 import { Converter } from "csvtojson";
 import { Relationships } from "../entity/Relationships";
 
-export let share = async (req: Request, res: Response){
+export let share = async (req: Request, res: Response) =>{
 
     const manager = getManager().getRepository(Relationships);
-    const data = await manager.query("SELECT COUNT(*) AS Share_Amount, Relationship_Type FROM relationships GROUP BY Relationship_Type")
+    const data = await manager.query("SELECT COUNT(*) AS Share_Count, Relationship_Type FROM relationships GROUP BY Relationship_Type")
     // console.log(data)
     res.send(data)
 }
 
-export let type = async (req: Request, res: Response){
+export let type = async (req: Request, res: Response)=>{
     var id = req.params.type;
-    console.log("------------------------")
-    console.log('id', id)
     const manager = getManager().getRepository(Relationships);
     const data = await manager.find({
         "Relationship_Type": id
     })
-    console.log(data)
+    // console.log(data)
     res.send(data)
 }
